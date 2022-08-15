4b1fdcaa4e6c9a486ab75c8a49aa72a5c530062f
losici <lore.sici@gmail.com>
2022-08-15 17:38:27 +0200

    Add conditional custom files in prj and Kconfig

--- fund_less3_exer1/src/main.c
+++ fund_less3_exer1/src/main.c
@@ -10,6 +10,11 @@ void main(void)
     int a = 3,  b = 4;
     while(1)
     {
+        if(!(IS_ENABLED(CONFIG_MYFUNCTION)))
+        {
+			printk("MYFUNCTION not enabled\r\n");
+			return;
+		}
         printk("The sum of %d and %d is %d\n\r", a, b, sum(a,b));
         k_msleep(SLEEP_TIME_MS);
     }
