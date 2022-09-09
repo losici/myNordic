9ded808ba478791729391352dbd746254a6e1131
losici <lore.sici@gmail.com>
2022-09-09 16:29:01 +0200

    Uart switches on Led with keyboards

---
+++ fund_less5_exer1/build/image_preload.cmake
@@ -0,0 +1,8 @@
+# Generated file that can be used to preload variant images
+set(BOARD "nrf52840dk_nrf52840" CACHE INTERNAL "NCS child image controlled")
+set(CACHED_CONF_FILE "C:/code/mynordic/myNordic/fund_less5_exer1/prj.conf" CACHE INTERNAL "NCS child image controlled")
+set(CONFIG_DEBUG_OPTIMIZATIONS "y" CACHE INTERNAL "NCS child image controlled")
+set(CONFIG_DEBUG_THREAD_INFO "y" CACHE INTERNAL "NCS child image controlled")
+set(DTC_OVERLAY_FILE "" CACHE INTERNAL "NCS child image controlled")
+set(NCS_TOOLCHAIN_VERSION "NONE" CACHE INTERNAL "NCS child image controlled")
+set(WEST_PYTHON "c:\ncs\toolchains\v2.0.0\opt\bin\python.exe" CACHE INTERNAL "NCS child image controlled")
